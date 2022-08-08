b4cccdfc5525463fe70eb95c2967b51755523548
SyrdiiDima <dimasyrdij@icloud.com>
2022-08-08 23:26:22 +0300

    upd start page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import { Container } from './Container/Container';
 import { Navigation } from './Navigation/Navigation';
 // import  Home  from 'pages/Home';
-import SharedLayout from './SharedLayout';
 // import Movies from '../pages/Movies';
 // import MovieInfo from '../pages/MovieInfo';
 // import Cast from '../pages/Cast';
@@ -18,21 +17,19 @@ const Reviews = lazy(() => import('../pages/Reviews'));
 
 export const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-
-          <Route path="movies" element={<Movies />} />
-
-          <Route path="movies/:id" element={<MovieInfo />}>
+    <Container>
+      <Navigation />
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<MovieInfo />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-
           <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
+        </Routes>
+      </Suspense>
+    </Container>
   );
 };
